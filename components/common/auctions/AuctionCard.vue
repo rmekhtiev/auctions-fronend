514027c37ca7804e360c99740829d5b853106691
Leonid Meleshin <lm@devolt.one>
2020-07-06 07:10:51 +0300

    fix(Auctions): card time attribute

--- components/common/auctions/AuctionCard.vue
+++ components/common/auctions/AuctionCard.vue
@@ -7,19 +7,19 @@
         <img
           class="object-cover object-center w-full h-56 lg:h-48 xl:h-56"
           src="https://via.placeholder.com/450x450"
-          :alt="auction.attributes.display_title"
+          :alt="auction.attributes.title"
         />
       </picture>
     </div>
     <h2 class="flex-grow px-6 pt-4 text-lg font-semibold text-gray-800">
-      {{ auction.attributes.display_title }}
+      {{ auction.attributes.title }}
     </h2>
     <ul class="px-6">
       <li class="flex items-center mt-2 text-gray-700">
         <clock-icon class="w-4 h-4" />
         <div
           class="px-2 text-sm"
-          v-text="$moment(auction.starts_at).format('LL')"
+          v-text="$moment(auction.attributes.starts_at).format('LL')"
         />
       </li>
       <li class="flex items-center mt-2 text-gray-700">
