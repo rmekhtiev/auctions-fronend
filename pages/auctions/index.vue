a32a4add2c6457fc58b08c83f7ada61283126c1e
Leonid Meleshin <lm@devolt.one>
2020-07-21 13:33:00 +0300

    feat(Auctions): mobile responsive paginations

--- pages/auctions/index.vue
+++ pages/auctions/index.vue
@@ -6,62 +6,89 @@
       :filters-options="filterOptions"
       :pending="$fetchState.pending"
     />
-    <div
-      v-if="lastMeta && lastMeta.page"
-      class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between"
-    >
-      <div>
-        <p class="text-sm leading-5 text-gray-700">
-          Показано с
-          <span class="font-medium">{{ lastMeta.page.from }}</span>
-          по
-          <span class="font-medium">{{ lastMeta.page.to }}</span>
-          из
-          <span class="font-medium">{{ lastMeta.page.total }}</span>
-          всего
-        </p>
+    <div v-if="lastMeta && lastMeta.page">
+      <div class="flex justify-between flex-1 sm:hidden">
+        <component
+          :is="$store.getters['auctions/hasPrevious'] ? 'nuxt-link' : 'button'"
+          :to="{ path: $route.path, query: { page: page - 1 } }"
+          :disabled="!$store.getters['auctions/hasPrevious']"
+          :class="{
+            'opacity-50 cursor-not-allowed': !$store.getters[
+              'auctions/hasPrevious'
+            ],
+          }"
+          class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700"
+        >
+          Назад
+        </component>
+        <component
+          :is="$store.getters['auctions/hasNext'] ? 'nuxt-link' : 'button'"
+          :to="{ path: $route.path, query: { page: page + 1 } }"
+          :disabled="!$store.getters['auctions/hasNext']"
+          :class="{
+            'opacity-50 cursor-not-allowed': !$store.getters[
+              'auctions/hasNext'
+            ],
+          }"
+          class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700"
+        >
+          Вперед
+        </component>
       </div>
-      <div>
-        <nav class="relative z-0 inline-flex shadow-sm">
-          <nuxt-link
-            v-if="$store.getters['auctions/hasPrevious']"
-            :to="{ path: $route.path, query: { page: page - 1 } }"
-            class="relative inline-flex items-center px-2 py-2 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-l-md hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
-            aria-label="Previous"
-          >
-            <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-              <path
-                fill-rule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </nuxt-link>
-          <component
-            :is="showPaginationItem(p) ? 'nuxt-link' : 'button'"
-            v-for="p in lastMeta.page['last-page']"
-            v-show="showPaginationItem(p) || showPaginationItem(p + 1)"
-            :key="`auctions-pagination-${p}`"
-            :to="{ path: $route.path, query: { page: p } }"
-            class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
-          >
-            {{ showPaginationItem(p) ? p : '...' }}
-          </component>
-          <nuxt-link
-            v-if="$store.getters['auctions/hasNext']"
-            :to="{ path: $route.path, query: { page: page + 1 } }"
-            class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-r-md hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
-            aria-label="Next"
-          >
-            <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </nuxt-link>
-        </nav>
+      <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+        <div>
+          <p class="text-sm leading-5 text-gray-700">
+            Показано с
+            <span class="font-medium">{{ lastMeta.page.from }}</span>
+            по
+            <span class="font-medium">{{ lastMeta.page.to }}</span>
+            из
+            <span class="font-medium">{{ lastMeta.page.total }}</span>
+            всего
+          </p>
+        </div>
+        <div>
+          <nav class="relative z-0 inline-flex shadow-sm">
+            <nuxt-link
+              v-if="$store.getters['auctions/hasPrevious']"
+              :to="{ path: $route.path, query: { page: page - 1 } }"
+              class="relative inline-flex items-center px-2 py-2 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-l-md hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
+              aria-label="Назад"
+            >
+              <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fill-rule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </nuxt-link>
+            <component
+              :is="showPaginationItem(p) ? 'nuxt-link' : 'button'"
+              v-for="p in lastMeta.page['last-page']"
+              v-show="showPaginationItem(p) || showPaginationItem(p + 1)"
+              :key="`auctions-pagination-${p}`"
+              :to="{ path: $route.path, query: { page: p } }"
+              class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+            >
+              {{ showPaginationItem(p) ? p : '...' }}
+            </component>
+            <nuxt-link
+              v-if="$store.getters['auctions/hasNext']"
+              :to="{ path: $route.path, query: { page: page + 1 } }"
+              class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-r-md hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500"
+              aria-label="Вперед"
+            >
+              <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </nuxt-link>
+          </nav>
+        </div>
       </div>
     </div>
   </div>
@@ -107,7 +134,7 @@ export default {
   }),
   computed: {
     page() {
-      return this.$route.query.page ?? 1
+      return parseInt(this.$route.query.page ?? 1)
     },
     pureFilter() {
       return omitby(this.filter, (value) => value === undefined)
